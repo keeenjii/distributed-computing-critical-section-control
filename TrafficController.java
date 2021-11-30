407d35e41af57e9d3707fe88ad8dacf0b609be65
Vinicius <vinicius.victor.girotto@gmail.com>
2021-11-29 22:35:11 -0300

    feito o exercicio

---
+++ TrafficController.java
@@ -0,0 +1,33 @@
+import java.util.concurrent.Semaphore;
+
+public class TrafficController {
+    private Semaphore sem = new Semaphore(1, true);
+
+    public void enterLeft() {
+        try {
+            sem.acquire();
+        } 
+        catch (InterruptedException e) { 
+            System.out.println("Excecao"); 
+        }
+
+    }
+
+    public void enterRight() {
+        try {
+            sem.acquire();
+        } 
+        catch (InterruptedException e) { 
+            System.out.println("Excecao"); 
+        }
+    }
+
+    public void leaveLeft() {
+        sem.release();
+    }
+
+    public void leaveRight() {
+        sem.release();
+    }
+
+}
